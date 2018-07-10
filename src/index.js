e62b2759251766a9bbe9be24c08f57febe5386a9
Christian Berger <christian.berger@gu.se>
2018-07-10 20:21:04 +0200

    * Code cleanup
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- src/index.js
+++ src/index.js
@@ -56,13 +56,10 @@ app.set('view engine', '.hbs');
 
 // Default landing page.
 app.get("/", function(req, res) {
-//    res.sendFile(path.join(__dirname + '/index.html'));
-console.log("main");
     res.render('main', { livePage: true });
 });
 
 app.get("/playback", function(req, res) {
-console.log("playback");
     res.render('main', { playbackPage: true });
 });
 
@@ -104,12 +101,12 @@ console.log(req.body);
     });
 });
 
-var replayRunning = false;
-var process_cluonreplay;
+var g_replayRunning = false;
+var g_cluonreplay;
 app.post('/replayrecfile', (req, res) => {
-    replayRunning = true;
-    process_cluonreplay = exec('cluon-replay --keeprunning --cid=' + PLAYBACK_OD4SESSION_CID + ' ' + req.body.recordingFileToPlay);
-    console.log('[opendlv-vehicle-view] Started cluon-replay, PID: ' + process_cluonreplay.pid);
+    g_replayRunning = true;
+    g_cluonreplay = exec('cluon-replay --keeprunning --cid=' + PLAYBACK_OD4SESSION_CID + ' ' + req.body.recordingFileToPlay);
+    console.log('[opendlv-vehicle-view] Started cluon-replay, PID: ' + g_cluonreplay.pid);
 
     res.send ({
         status      : "200",
@@ -118,20 +115,15 @@ app.post('/replayrecfile', (req, res) => {
     });
 });
 app.post('/endreplay', (req, res) => {
-    try {
-        kill(process_cluonreplay.pid);
-        console.log('[opendlv-vehicle-view] Stopped cluon-replay, PID: ' + process_cluonreplay.pid);
-    }
-    catch (e) {
-        console.log(e);
-    }
+    try { kill(g_cluonreplay.pid); } catch (e) { console.log(e); }
+    console.log('[opendlv-vehicle-view] Stopped cluon-replay, PID: ' + g_cluonreplay.pid);
 
     res.send ({
         status      : "200",
         responseType: "string",
         response    : "success"
     });
-    replayRunning = false;
+    g_replayRunning = false;
 });
 app.post('/deleterecfile', (req, res) => {
     fs.unlink(req.body.recordingFileToDelete, function() {
@@ -145,8 +137,7 @@ app.post('/deleterecfile', (req, res) => {
 
 //------------------------------------------------------------------------------
 // Serve other static files.
-app.get(/^(.+)$/, function(req, res){ 
-console.log("catch all: " + req.params[0]);
+app.get(/^(.+)$/, function(req, res){
     res.sendFile(path.join(__dirname + '/' + req.params[0]));
 });
 
@@ -158,43 +149,39 @@ var server = app.listen(PORT, function () {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Websocket stuff.
-var process_cluonOD4toStdout;
+var g_cluonOD4toStdout;
 const WebSocket = require('ws').Server;
-const ws = new WebSocket({server});
-ws.on('connection', function connection(ws) {
-    ws.on('message', function(msg) {
-        if ( (msg[0] == '{') && (msg[msg.length-1] == '}') ) {
+const g_ws = new WebSocket({server});
+g_ws.on('connection', function connection(conn) {
+    conn.on('message', function(msg) {
+        if ( /* Ensure we have pure JSON. */ (msg[0] == '{') && (msg[msg.length-1] == '}') ) {
             var data = JSON.parse(msg);
             Object.keys(data).forEach(function(key) {
                 if ('record' == key) {
                     if (data.record) {
-                        process_cluonOD4toStdout = exec('cluon-OD4toStdout --cid=' + LIVE_OD4SESSION_CID + ' > ./recordings/`date +CID-' + LIVE_OD4SESSION_CID + '-recording-%Y-%m-%d_%H%M%S.rec`');
-                        console.log('[opendlv-vehicle-view] Started cluon-OD4toStdout, PID: ' + process_cluonOD4toStdout.pid);
+                        g_cluonOD4toStdout = exec('cluon-OD4toStdout --cid=' + LIVE_OD4SESSION_CID + ' > ./recordings/`date +CID-' + LIVE_OD4SESSION_CID + '-recording-%Y-%m-%d_%H%M%S.rec`');
+                        console.log('[opendlv-vehicle-view] Started cluon-OD4toStdout, PID: ' + g_cluonOD4toStdout.pid);
                     }
                     else {
-                        try {
-                            kill(process_cluonOD4toStdout.pid);
-                        }
-                        catch (e) {
-                            console.log(e);
-                        }
-                        console.log('[opendlv-vehicle-view] Stopped cluon-OD4toStdout, PID: ' + process_cluonOD4toStdout.pid);
+                        try { kill(g_cluonOD4toStdout.pid); } catch (e) { console.log(e); }
+                        console.log('[opendlv-vehicle-view] Stopped cluon-OD4toStdout, PID: ' + g_cluonOD4toStdout.pid);
                     }
                 }
                 if ('remoteplayback' == key) {
-                    // Forward command to playback OD4Session.
-                    var buf = Buffer.from(data.remoteplayback, 'base64');
-                    playbackOD4Session.send(buf, 12175, '225.0.0.' + PLAYBACK_OD4SESSION_CID);
+                    // Unpack Proto-encoded Envelope and forward command to playback OD4Session.
+                    g_playbackOD4Session.send(Buffer.from(data.remoteplayback, 'base64'), 12175, '225.0.0.' + PLAYBACK_OD4SESSION_CID);
                 }
             });
         }
     });
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// Broadcast to connected websocket clients.
 var broadcastMessage = function (msg, fromLive) {
-    if ( ( (fromLive && !replayRunning) /* from live OD4Session */ ) ||
-         ( (!fromLive && replayRunning) /* from replay OD4Session */ ) ) {
-        ws.clients.forEach(function each(client) {
+    if ( ( (fromLive && !g_replayRunning) /* Forward either from live OD4Session */ ) ||
+         ( (!fromLive && g_replayRunning) /* or from replay OD4Session but not from both. */ ) ) {
+        g_ws.clients.forEach(function each(client) {
             if (client.readyState == 1 /*WebSocket.OPEN*/) {
                 client.send(msg);
             }
@@ -204,23 +191,23 @@ var broadcastMessage = function (msg, fromLive) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Connect to live OD4Session to broadcast messages to connected websocket clients.
-var liveOD4Session = dgram.createSocket({reuseAddr:true, type:'udp4'});
-liveOD4Session.bind({ 'port' : 12175 /* OD4Session UDP multicast port */, 'address': '225.0.0.' + LIVE_OD4SESSION_CID, 'exclusive' : false });
-liveOD4Session.on('listening', function() {
-    liveOD4Session.addMembership('225.0.0.' + LIVE_OD4SESSION_CID);
+var g_liveOD4Session = dgram.createSocket({reuseAddr:true, type:'udp4'});
+g_liveOD4Session.bind({ 'port' : 12175 /* OD4Session UDP multicast port */, 'address': '225.0.0.' + LIVE_OD4SESSION_CID, 'exclusive' : false });
+g_liveOD4Session.on('listening', function() {
+    g_liveOD4Session.addMembership('225.0.0.' + LIVE_OD4SESSION_CID);
 });
-liveOD4Session.on('message', function(msg, rinfo) {
+g_liveOD4Session.on('message', function(msg, rinfo) {
     broadcastMessage(msg, true);
 });
 
 ////////////////////////////////////////////////////////////////////////////////
 // Connect to playback OD4Session to broadcast messages to connected websocket clients.
-var playbackOD4Session = dgram.createSocket({reuseAddr:true, type:'udp4'});
-playbackOD4Session.bind({ 'port' : 12175 /* OD4Session UDP multicast port */, 'address': '225.0.0.' + PLAYBACK_OD4SESSION_CID, 'exclusive' : false });
-playbackOD4Session.on('listening', function() {
-    playbackOD4Session.addMembership('225.0.0.' + PLAYBACK_OD4SESSION_CID);
+var g_playbackOD4Session = dgram.createSocket({reuseAddr:true, type:'udp4'});
+g_playbackOD4Session.bind({ 'port' : 12175 /* OD4Session UDP multicast port */, 'address': '225.0.0.' + PLAYBACK_OD4SESSION_CID, 'exclusive' : false });
+g_playbackOD4Session.on('listening', function() {
+    g_playbackOD4Session.addMembership('225.0.0.' + PLAYBACK_OD4SESSION_CID);
 });
-playbackOD4Session.on('message', function(msg, rinfo) {
+g_playbackOD4Session.on('message', function(msg, rinfo) {
     broadcastMessage(msg, false);
 });
 
